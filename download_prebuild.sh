7ba3f99f208a2e9fe152b66e00228d49ac1d6d24
wangsijie <wangsijie@bytedance.com>
2018-04-02 10:20:31 +0800

    refactor rename repo
    Change-Id: I4f9dbdf3ad09ae54388e63e0731061636c7ddf93

--- download_prebuild.sh
+++ download_prebuild.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo wget https://github.com/xinghun92/lark_log/raw/master/prebuild/lark_log -O /usr/bin/lark_log
+sudo wget https://github.com/xinghun92/lark_log/raw/master/prebuild/parse_log -O /usr/bin/parse_log
+sudo chmod a+x /usr/bin/parse_log
