bb160e66f53789ad6a9aedb8bb8b3d1caf5e3dae
xinghun92 <345954715@qq.com>
2018-05-25 17:13:49 +0800

    Update download_prebuild.sh

--- download_prebuild.sh
+++ download_prebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo wget https://github.com/xinghun92/lark_log/raw/master/prebuild/parse_log -O /usr/bin/parse_log
-sudo chmod a+x /usr/bin/parse_log
+sudo wget https://github.com/xinghun92/lark_log/raw/master/prebuild/parse_log -O /usr/local/bin/parse_log
+sudo chmod a+x /usr/local/bin/parse_log
